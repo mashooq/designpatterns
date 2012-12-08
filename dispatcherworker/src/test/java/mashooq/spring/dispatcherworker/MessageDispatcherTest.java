d101c51a9cd4621d170a2af12fd7a0a62c0718a8
Mash <mashooq.badar@gmail.com>
2012-12-08 14:36:46 +0000

    refactored test for dispatcher

--- dispatcherworker/src/test/java/mashooq/spring/dispatcherworker/MessageDispatcherTest.java
+++ dispatcherworker/src/test/java/mashooq/spring/dispatcherworker/MessageDispatcherTest.java
@@ -2,54 +2,50 @@ package mashooq.spring.dispatcherworker;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.integration.MessageChannel;
-import org.springframework.integration.channel.QueueChannel;
-import org.springframework.integration.endpoint.PollingConsumer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static com.jayway.awaitility.Awaitility.to;
+import static com.jayway.awaitility.Awaitility.waitAtMost;
+import static com.jayway.awaitility.Duration.FIVE_SECONDS;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 import static org.springframework.integration.support.MessageBuilder.withPayload;
 
 @ContextConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
-public class MessageDispatcherTest implements ApplicationContextAware {
-	
-	@Autowired
-	MessageChannel inputChannel;
-    private ApplicationContext applicationContext;
+public class MessageDispatcherTest {
+
+    public static final String ID_ONE = "id-1";
+    public static final String ID_TWO = "id-2";
+    public static final String MESSAGE_PAYLOAD = "sample message payload";
+    @Autowired
+    MessageChannel inputChannel;
+
+    @Autowired
+    private MessageDispatcher messageDispatcher;
 
 
     @Test
-	public void testCat() throws InterruptedException {
-		inputChannel.send(withPayload(createMessage("id-1", 1)).build());
-		inputChannel.send(withPayload(createMessage("id-1", 5)).build());
-		inputChannel.send(withPayload(createMessage("id-2", 1)).build());
-		inputChannel.send(withPayload(createMessage("id-1", 2)).build());
+    public void testCat() throws Exception {
+        inputChannel.send(withPayload(createMessage(ID_ONE, 1)).build());
+        inputChannel.send(withPayload(createMessage(ID_ONE, 5)).build());
+        inputChannel.send(withPayload(createMessage(ID_TWO, 1)).build());
+        inputChannel.send(withPayload(createMessage(ID_ONE, 2)).build());
 
-        MessageDispatcher messageDispatcher = (MessageDispatcher) applicationContext.getBean("messageDispatcher");
-        assertNotNull(messageDispatcher);
-        Thread.sleep(1000);
-        assertThat(messageDispatcher.getNumberOfVersionsReceivedForMessage("id-1"), is(3));
-        assertThat(messageDispatcher.getNumberOfVersionsReceivedForMessage("id-2"), is(1));
-	}
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+        final int expectedNumberOfVersions = 3;
+        waitAtMost(FIVE_SECONDS).untilCall(
+                to(messageDispatcher).getNumberOfVersionsReceivedForMessage(ID_ONE),
+                is(expectedNumberOfVersions));
     }
-    
+
+
     private CustomMessage createMessage(String id, Integer version) {
         CustomMessage message = new CustomMessage();
         message.setId(id);
         message.setVersion(version);
-        message.setPayload("message body");
+        message.setPayload(MESSAGE_PAYLOAD);
         return message;
     }
 }
